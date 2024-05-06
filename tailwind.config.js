be3fd4635d3e6573d0d0f62d0127102a91421317
adriangutierrezd <adriangutierrezdelaosa@gmail.com>
2024-05-06 20:16:36 +0200

    Primer commit

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+
+import daisyui from "daisyui"
+
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    daisyui
+  ],
+  daisyui: {
+    themes: [
+      "light",
+      "dark",
+      "forest",
+      "cyberpunk",
+      "emerald",
+    ],
+  },
+}
